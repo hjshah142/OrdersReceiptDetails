8921380139ceebac63ee278a50f3aaaaa6857bd5
Harsh Shah <harshshah142@gmail.com>
2022-11-27 16:37:53 +0100

    Project Setup JavaSE-1.8

---
+++ src/order/receipt/OrdersReceiptDetails.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package order.receipt;
+
+/**
+ * @author Shah Harsh
+ *
+ */
+public class OrdersReceiptDetails {
+
+	/**
+	 * 
+	 */
+	public OrdersReceiptDetails() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Order Details");
+		// TODO Auto-generated method stub
+
+	}
+
+}
