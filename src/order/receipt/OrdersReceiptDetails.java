ab0df404dd32a9bc1e2df2276ec29f5baf3ca497
Harsh Shah <harshshah142@gmail.com>
2022-11-28 20:50:02 +0100

    Calcluation of each item taxes in seprate funtion

--- src/order/receipt/OrdersReceiptDetails.java
+++ src/order/receipt/OrdersReceiptDetails.java
@@ -19,17 +19,18 @@ import order.receipt.items.Item;
 public class OrdersReceiptDetails {
 
 	/**
-	 * 
+	 * Class calculates the taxes for the items and prints order summary
 	 */
 	double imported_tax_rate = 0.05;
     double sales_tax_rate = 0.10;
-	private static Double TotalSalesTax = 0.00;
+    private static Double TotalSalesTax= 0.00;
 	private static Double TotalAmount= 0.00;
 	private static ArrayList<Item> ItemsList = new ArrayList<>();
 	
 	
 	
 	public OrdersReceiptDetails() {
+		
 		// TODO Auto-generated constructor stub
 	}
 
@@ -52,7 +53,7 @@ public class OrdersReceiptDetails {
 			//System.out.println(item_qty);
 			String item_price_string = item_order_words.get(item_order_words.size()-1);
 		    double item_price =  Double.parseDouble(item_price_string); 
-			System.out.println(item_price);
+			//System.out.println(item_price);
 			String item_name = item_order.replace(item_price_string, "");
 			item_name = item_name.replace(" at", "");
 			// System.out.println(item_name); 
@@ -72,38 +73,22 @@ public class OrdersReceiptDetails {
 			
 		}
 		// calculating tax for each items and printing order summary
-		calculateTax();
+		printOrderSummary();
 
 
 	}
 	
 
 
-	private static void calculateTax() {
-		Double TotalAmount = 0.00;
-		Double TotalSalesTax =0.00; 
+	private static void printOrderSummary() {
+
 		// TODO Auto-generated method stub
         for (Item item : ItemsList) {
         	    	
         	
-        	double item_price = item.getItem_price();
-        	//System.out.println(item_price);
-        	double item_cost = item_price * item.getItem_qty();
-        	//System.out.println(item_cost);
-        	double salesTax = 0.00;
-        	double importDuty = 0.00;
-        	
-        	if(!item.isItemExempted()) {
-        		salesTax = item_cost * 0.10;
-        		//salesTax = roundDoubleValue(salesTax);
-        		
-        	}
-        	if(item.isItemImported()) {
-        		importDuty = item_cost *0.05;
-        		//importDuty = roundDoubleValue(importDuty);
-        	}
-        	double itemTax = salesTax + importDuty;
-        	double itemTaxRounded = roundDoubleValue(itemTax);
+        	// calculate tax for the item
+        	double itemTaxRounded = calculateTax(item);
+        	double item_cost = item.getItem_price();
         	double itemCostwithTax = itemTaxRounded + item_cost;
         	// double itemCostwithTaxRounded = roundDoubleValue(itemCostwithTax);
         	
@@ -114,8 +99,7 @@ public class OrdersReceiptDetails {
         	TotalAmount = itemCostwithTax + TotalAmount;
         	
         	System.out.printf("%s: %.2f  \n",item.getItem_name(), item.getItem_price());
-        	
-        	
+
         }
         System.out.printf("Sales Taxes: %f",TotalSalesTax);
         System.out.printf("\nTotal: %.2f",TotalAmount);
@@ -126,6 +110,31 @@ public class OrdersReceiptDetails {
 
 	
 
+	private static double calculateTax(Item item) {
+		
+		// TODO Auto-generated method stub
+		double salesTax = 0.00;
+    	double importDuty = 0.00;
+       	double item_price = item.getItem_price();
+    	//System.out.println(item_price);
+    	double item_cost = item_price * item.getItem_qty();
+    	item.setItem_price(item_cost);
+    	
+    	if(!item.isItemExempted()) {
+    		salesTax = item_cost * 0.10;
+    		//salesTax = roundDoubleValue(salesTax);
+    		
+    	}
+    	if(item.isItemImported()) {
+    		importDuty = item_cost *0.05;
+    		//importDuty = roundDoubleValue(importDuty);
+    	}
+    	double itemTax = salesTax + importDuty;
+    	double itemTaxRounded = roundDoubleValue(itemTax);
+    	return itemTaxRounded;
+		
+	}
+
 	private static double roundDoubleValue(double value) {
 		// TODO Auto-generated method stub
 		double rounded = Math.round(value * 20.0) / 20.0;
@@ -134,7 +143,7 @@ public class OrdersReceiptDetails {
 
 	private static List<String> readItemDetails(String input_file_name) throws IOException {
 		// TODO Auto-generated method stub
-		Path fileName= Path.of("OrderData/Input2.txt");
+		Path fileName= Path.of("OrderData/Input3.txt");
 		String fileContent = Files.readString(fileName);
 		//System.out.println(fileContent);
 		List<String> items = Arrays.asList(fileContent.split("\n"));
