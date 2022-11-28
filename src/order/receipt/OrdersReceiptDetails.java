7a63a1e7eb8daad94751ed82e10c3eeb54e7f0ea
Harsh Shah <harshshah142@gmail.com>
2022-11-28 05:07:52 +0100

    Calcuating tax and priting orders summary

--- src/order/receipt/OrdersReceiptDetails.java
+++ src/order/receipt/OrdersReceiptDetails.java
@@ -3,6 +3,15 @@
  */
 package order.receipt;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import order.receipt.items.Item;
+
 /**
  * @author Shah Harsh
  *
@@ -12,18 +21,125 @@ public class OrdersReceiptDetails {
 	/**
 	 * 
 	 */
+	double imported_tax_rate = 0.05;
+    double sales_tax_rate = 0.10;
+	private static Double TotalSalesTax = 0.00;
+	private static Double TotalAmount= 0.00;
+	private static ArrayList<Item> ItemsList = new ArrayList<>();
+	
+	
+	
 	public OrdersReceiptDetails() {
 		// TODO Auto-generated constructor stub
 	}
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		System.out.println("Order Details");
-		// TODO Auto-generated method stub
+		// System.out.println("Order Details");
+		String input_file_name = "Input2.txt";
+		List<String> items =  readItemDetails(input_file_name);
+		// System.out.println(items.toString());
+		for(int i=0; i< items.size();i++)
+		{
+			String item_order = items.get(i);
+			// System.out.print(item_order);
+			List<String> item_order_words =  Arrays.asList(item_order.split(" "));
+			int item_qty = Integer.parseInt(item_order_words.get(0));
+			//System.out.println(item_qty);
+			String item_price_string = item_order_words.get(item_order_words.size()-1);
+		    double item_price =  Double.parseDouble(item_price_string); 
+			System.out.println(item_price);
+			String item_name = item_order.replace(item_price_string, "");
+			item_name = item_name.replace(" at", "");
+			// System.out.println(item_name); 
+			Item item_obj; 
+			
 
+			item_obj = new Item(item_qty, item_name, item_price);
+			if(item_name.contains("imported")) {
+				item_obj.setItemImported(true);
+			}
+			if(item_name.contains("book") || item_name.contains("chocolate") || item_name.contains("pills")) {
+				item_obj.setItemExempted(true);
+			}
+
+			ItemsList.add(item_obj);
+
+			
+		}
+		// calculating tax for each items and printing order summary
+		calculateTax();
+
+
+	}
+	
+
+
+	private static void calculateTax() {
+		Double TotalAmount = 0.00;
+		Double TotalSalesTax =0.00; 
+		// TODO Auto-generated method stub
+        for (Item item : ItemsList) {
+        	    	
+        	
+        	double item_price = item.getItem_price();
+        	//System.out.println(item_price);
+        	double item_cost = item_price * item.getItem_qty();
+        	//System.out.println(item_cost);
+        	double salesTax = 0.00;
+        	double importDuty = 0.00;
+        	
+        	if(!item.isItemExempted()) {
+        		salesTax = item_cost * 0.10;
+        		//salesTax = roundDoubleValue(salesTax);
+        		
+        	}
+        	if(item.isItemImported()) {
+        		importDuty = item_cost *0.05;
+        		//importDuty = roundDoubleValue(importDuty);
+        	}
+        	double itemTax = salesTax + importDuty;
+        	double itemTaxRounded = roundDoubleValue(itemTax);
+        	double itemCostwithTax = itemTaxRounded + item_cost;
+        	// double itemCostwithTaxRounded = roundDoubleValue(itemCostwithTax);
+        	
+        	// Set item cost after calculating tax 
+        	item.setItem_price(itemCostwithTax);
+        	TotalSalesTax = itemTaxRounded + TotalSalesTax;
+        	//TotalSalesTax = roundDoubleValue(TotalSalesTax);
+        	TotalAmount = itemCostwithTax + TotalAmount;
+        	
+        	System.out.printf("%s: %.2f  \n",item.getItem_name(), item.getItem_price());
+        	
+        	
+        }
+        System.out.printf("Sales Taxes: %f",TotalSalesTax);
+        System.out.printf("\nTotal: %.2f",TotalAmount);
+	
+	}
+	
+
+
+	
+
+	private static double roundDoubleValue(double value) {
+		// TODO Auto-generated method stub
+		double rounded = Math.round(value * 20.0) / 20.0;
+		return rounded;
+	}
+
+	private static List<String> readItemDetails(String input_file_name) throws IOException {
+		// TODO Auto-generated method stub
+		Path fileName= Path.of("OrderData/Input2.txt");
+		String fileContent = Files.readString(fileName);
+		//System.out.println(fileContent);
+		List<String> items = Arrays.asList(fileContent.split("\n"));
+		return items;
+		
 	}
 
 }
---
+++ src/order/receipt/items/Item.java
@@ -0,0 +1,59 @@
+package order.receipt.items;
+
+public class Item {
+	private int item_qty; 
+	private String item_name; 
+	private double item_price;
+	private boolean itemExempted= false;
+	private boolean itemImported= false;
+	
+	public Item(int qty, String name, double price){
+		this.item_qty = qty;
+		this.item_price = price;
+		this.item_name = name;
+       
+    }
+
+	public int getItem_qty() {
+		return item_qty;
+	}
+
+	public void setItem_qty(int item_qty) {
+		this.item_qty = item_qty;
+	}
+
+	public String getItem_name() {
+		return item_name;
+	}
+
+	public void setItem_name(String item_name) {
+		this.item_name = item_name;
+	}
+
+	public double getItem_price() {
+		return item_price;
+	}
+
+	public void setItem_price(double item_price) {
+		this.item_price = item_price;
+	}
+
+	public boolean isItemExempted() {
+		return itemExempted;
+	}
+
+	public void setItemExempted(boolean itemExempted) {
+		this.itemExempted = itemExempted;
+	}
+
+	public boolean isItemImported() {
+		return itemImported;
+	}
+
+	public void setItemImported(boolean itemImported) {
+		this.itemImported = itemImported;
+	}
+	
+	
+
+}
